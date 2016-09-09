e36aa0bdfd02198a550e47eba748b7a2bdd5306f
cmun777 <cmun777@gmail.com>
2016-09-09 15:22:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-//Create a bot on dev.groupme.com, assign it to the group you want it to live in
-//Login in to your heroku account and input the Bot ID as your config variable named BOT_ID
-//Type /g "Search Term" to display Gifs in your chat
-
 var HTTPS = require('https');
 var request = require('request');
 var botID = process.env.BOT_ID;
@@ -9,9 +5,6 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
   trigger = request.text.substring(0,2);
-  //establishes the "trigger zone" as the first 2 characters of every message
-  //gifbot will now scan the first two characters of every message for the trigger that you establish.
-  // if you want to use a trigger longer than 2 characters you will need to change the above substring command
   
   if (trigger == '/g' && request.name != 'gifbot') {
     searchTerm = request.text.substr(3);
